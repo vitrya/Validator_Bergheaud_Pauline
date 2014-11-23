7083cc2642ede0df7d41f80a00ed0f013317d8fe
Pauline BERGHEAUD <pauline.bergheaud@devinci.fr>
2014-11-23 15:26:05 +0100

    false

--- src/IntegerValidator.php
+++ src/IntegerValidator.php
@@ -12,8 +12,8 @@ namespace Pauline\Validator;
 class IntegerValidator {
 
     public static function equal ($integer, $numbre) {
-        if (is_int ($integer) === False) throw new \Exception('Integer not integer');
-        if (is_int ($numbre) === False) throw new \Exception('Number not integer');
+        if (is_int ($integer) === false) throw new \Exception('Integer not integer');
+        if (is_int ($numbre) === false) throw new \Exception('Number not integer');
         $bool = ($numbre === $integer) ? 'True' : 'False';
         //$bool = ($numbre === $integer) ? True : False;
         return $bool;
