ad1e4781ab73cfc663384ec0a65777cae1ca2681
Pauline BERGHEAUD <pauline.bergheaud@devinci.fr>
2014-11-23 15:12:01 +0100

    test up

--- tests/IntegerValidatorTest.php
+++ tests/IntegerValidatorTest.php
@@ -14,15 +14,15 @@ class IntegerValidatorTest extends \PHPUnit_Framework_TestCase {
 
     public function testIntegerValidatorEqual()
     {
-        $number = mt_rand(1,1000);
+        $number = mt_rand (1,1000);
         $integer = IntegerValidator::equal($number,$number);
-        $this->assertEquals('True', $integer);
+        $this->assertEquals ('True', $integer);
     }
     public function testIntegerValidatorNotEqual()
     {
-        $number = mt_rand(1,1000);
-        $number2 = mt_rand(-1000,0);
-        $integer = IntegerValidator::equal($number,$number2);
-        $this->assertEquals('False', $integer);
+        $number = mt_rand (1,1000);
+        $number2 = mt_rand (-1000,0);
+        $integer = IntegerValidator::equal ($number,$number2);
+        $this->assertEquals ('False', $integer);
     }
 }
